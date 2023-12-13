95b4bd29873ef3ca49977ebf9c41da2b30703328
Santeri Uusitalo <vs.uusitalo@gmail.com>
2023-12-13 13:58:32 +0200

    checkout form started

--- react-dine-frontend/src/components/CheckoutForm.jsx
+++ react-dine-frontend/src/components/CheckoutForm.jsx
@@ -1,4 +1,5 @@
 import { useForm } from 'react-hook-form'
+import './CheckoutForm.css'
 
 const CheckoutForm = () => {
     const { register, handleSubmit, watch, formState: { errors },} = useForm();
@@ -8,18 +9,50 @@ const CheckoutForm = () => {
     }
 
     return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input defaultValue="First Name" {...register("firstname",{ required: 'error message'})}/>
-      <input defaultValue="Last Name" {...register("lastname",{ required: true})}/>
-      <input defaultValue="Address" {...register("address",{ required: true})}/>
-      <input defaultValue="Phone number" {...register("phonenumber",{ required: true})}/>
-      <input defaultValue="Special instructions" {...register("specialinstructions",{ maxLength: 100})}/>
+        <div className="checkoutform">
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="form-control">
+                    <label>First Name:</label>
+                    <input type="text" {...register("firstname",{ required: true})}/>
+                    {errors.firstname && errors.firstname.type === "required" && (
+                        <p className="errorMsg"> First Name is required!</p>
+                    )}
+                </div>
+                <div className="form-control">
+                    <label>Last Name:</label>
+                    <input type="text" {...register("lastname",{ required: true})}/>
+                    {errors.lastname && errors.lastname.type === "required" && (
+                        <p className="errorMsg"> Last Name is required!</p>
+                    )}
+                </div>
+                <div className="form-control">
+                    <label>Address:</label>
+                    <input {...register("address",{ required: true})}/>
+                    {errors.address && errors.address.type === "required" && (
+                        <p className="errorMsg"> Address is required!</p>
+                    )}
+                </div>
+                <div className="form-control">
+                    <label>Phone number:</label>
+                    <input type="number" {...register("phonenumber",{ required: true})}/>
+                    {errors.phonenumber && errors.phonenumber.type === "required" && (
+                        <p className="errorMsg"> Phonenumber is required!</p>
+                    )}
+                </div>
+                <div className="form-control-spec">
+                    <label>Special instructions:</label>
+                    <textarea type="text" rows="4" {...register("specialinstructions",{ maxLength: 150})}/>
+                    {errors.specialinstructions && errors.specialinstructions.type === "maxLength" && (
+                        <p className="errorMsg"> Must be under 150 characters!</p>
+                    )}
+                </div>
 
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
+                {/* errors will return when field validation fails  */}
+                {errors.exampleRequired && <span>This field is required</span>}
 
-      <input type="submit" />
-    </form>
+                <input type="submit" />
+            </form>
+        </div>
   )
 };
 
