fbb839e449e817a02ce1b773d7a5fac883215d06
Santeri Uusitalo <vs.uusitalo@gmail.com>
2023-12-15 19:25:05 +0200

    improved loader

--- react-dine-frontend/src/components/Loader.jsx
+++ react-dine-frontend/src/components/Loader.jsx
@@ -4,7 +4,7 @@ const Loader = () => {
     return(
         <div className="loader">
             <h2>Fetching data...</h2>
-            <h3>Try refreshing the page.</h3>
+            <h3>Please wait patiently. 🙏</h3>
         </div>
     )
 }
