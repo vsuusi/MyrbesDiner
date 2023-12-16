ec08ebeae1dfb15efffd0de75a153ec85a191f30
Santeri Uusitalo <vs.uusitalo@gmail.com>
2023-12-17 01:13:07 +0200

    small adjustments to loader and checkoutform

--- react-dine-frontend/src/components/Loader.jsx
+++ react-dine-frontend/src/components/Loader.jsx
@@ -5,6 +5,7 @@ const Loader = () => {
         <div className="loader">
             <h2>Fetching data...</h2>
             <h3>Please wait patiently. 🙏</h3>
+            <h4>The backend is slow to load because of the free tier, may take up to a minute.</h4>
         </div>
     )
 }
