3a864ae3ee94998d06239ab6e3956bbe9d91f9a2
Santeri Uusitalo <vs.uusitalo@gmail.com>
2024-01-23 18:33:15 +0200

    added a animation for loader

--- react-dine-frontend/src/components/Loader.jsx
+++ react-dine-frontend/src/components/Loader.jsx
@@ -1,11 +1,14 @@
 import './Loader.css'
+import { BounceLoader, ClipLoader } from 'react-spinners';
 
 const Loader = () => {
     return(
         <div className="loader">
-            <h2>Fetching data...</h2>
-            <h3>Please wait patiently. 🙏</h3>
-            <h4>Loading the page for the first time may take up to a minute.</h4>
+            <h2>Fetching data, please wait.</h2>
+            <BounceLoader className='loader-animation'
+                size={50}
+            />
+            <h4>Loading the page for the first time may take up to a few minutes.</h4>
         </div>
     )
 }
